3871a4942ff21bc7d418029579b596d1a0973335
Michel Belleville <michel.belleville@gmail.com>
2009-02-11 18:29:29 +0100

    Destroying the parent destroys the children.

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
 
   belongs_to :parent,   :class_name => 'Item', :foreign_key => 'parent_id'
   has_many   :children, :class_name => 'Item', :foreign_key => 'parent_id',
-             :order => 'rank ASC'
+             :dependent => :destroy, :order => 'rank ASC'
 
   named_scope :ordered, :order => 'rank ASC'
   named_scope :with_children, :include => :children do
