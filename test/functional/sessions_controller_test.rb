ba092fe6be89f661f1e4448779ba8abd09b21b8f
Michel Belleville <michel.belleville@gmail.com>
2009-02-11 17:04:52 +0100

    Small cleanup in functional sessions controller test.

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,18 +1,12 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:sessions)
-  end
-
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should create session" do
+  test "should create session" do # FIXME
     assert_difference('Session.count') do
       post :create, :session => { }
     end
@@ -20,24 +14,9 @@ class SessionsControllerTest < ActionController::TestCase
     assert_redirected_to session_path(assigns(:session))
   end
 
-  test "should show session" do
-    get :show, :id => sessions(:one).id
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => sessions(:one).id
-    assert_response :success
-  end
-
-  test "should update session" do
-    put :update, :id => sessions(:one).id, :session => { }
-    assert_redirected_to session_path(assigns(:session))
-  end
-
-  test "should destroy session" do
+  test "should destroy session" do # FIXME
     assert_difference('Session.count', -1) do
-      delete :destroy, :id => sessions(:one).id
+      delete :destroy
     end
 
     assert_redirected_to sessions_path
