ea55a768eb49c654a8727ed0f99365b5acbf4e1c
Michel Belleville <michel.belleville@gmail.com>
2009-02-11 17:01:58 +0100

    Default sort order for children.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ActiveRecord::Base
   validates_presence_of :rank, :on => :update
 
   belongs_to :parent,   :class_name => 'Item', :foreign_key => 'parent_id'
-  has_many   :children, :class_name => 'Item', :foreign_key => 'parent_id'
+  has_many   :children, :class_name => 'Item', :foreign_key => 'parent_id',
+             :order => 'rank ASC'
 
   named_scope :ordered, :order => 'rank ASC'
   named_scope :with_children, :include => :children do
